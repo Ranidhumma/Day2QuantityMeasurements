6d6e07bb28cf304d5b6de043ad97a4e85019dac6
Ranidhumma <ranidhumma8@gmail.com>
2022-04-18 23:13:49 +0530

    TC1.2 performing test for Null check

--- src/test/java/com/java/QuantityTest.java
+++ src/test/java/com/java/QuantityTest.java
@@ -19,4 +19,16 @@ public class QuantityTest {
 		Feet feet2 = new Feet(0.0);
 		Assert.assertEquals(feet1, feet2);
 	}
+
+	/* Tc 1.2 - Null check */
+
+	@Test
+	public void given1feetandNullShouldReturNotEqual() {
+		Feet feet1 = new Feet(1.0);
+		Feet feet2 = null;
+		// Assert.assertNotEquals(feet1, feet2);
+		// Assert.assertNotNull(feet1);
+		Assert.assertFalse(feet1.equals(feet2));
+
+	}
 }
