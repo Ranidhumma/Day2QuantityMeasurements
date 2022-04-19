4dbd523c5170566b0cf5656cc1a2c7392f6a92ad
Ranidhumma <ranidhumma8@gmail.com>
2022-04-19 10:47:29 +0530

    Reference checking, for same it returns True , for diff returning False

--- src/test/java/com/java/QuantityTest.java
+++ src/test/java/com/java/QuantityTest.java
@@ -20,15 +20,31 @@ public class QuantityTest {
 		Assert.assertEquals(feet1, feet2);
 	}
 
-	/* Tc 1.2 - Null check */
-
+	/* Tc 1.2 - Null checking */
 	@Test
 	public void given1feetandNullShouldReturNotEqual() {
-		Feet feet1 = new Feet(1.0);
-		Feet feet2 = null;
-		// Assert.assertNotEquals(feet1, feet2);
+		Feet feet1 = null;
+		Feet feet2 = new Feet(1.0);
+		Assert.assertNotEquals(feet1, feet2);
 		// Assert.assertNotNull(feet1);
-		Assert.assertFalse(feet1.equals(feet2));
+		// Assert.assertFalse(feet1.equals(feet2))
+		// Assert.assertNull(null, feet1);
+	}
 
+	/* TC 1.3 Same Reference type */
+	@Test
+	public void given1feetand1feet_WhenEqual_SameRefType_ShouldReturnTrue() {
+		Feet feet1 = new Feet(1.0);
+		boolean result = (feet1.equals(feet1));
+		Assert.assertTrue(result);
+	}
+
+	/* Diff Reference type */
+	@Test
+	public void given1feetand0feet_WhenEqual_DiffRefType_ShouldReturnFalse() {
+		Feet feet1 = new Feet(1.0);
+		Feet feet2 = new Feet(0.0);
+		boolean result = (feet1.equals(feet2));
+		Assert.assertFalse(result);
 	}
 }
