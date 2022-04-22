9bf4f04e3f57a3e2213a6889489ad3f2639a0959
Ranidhumma <ranidhumma8@gmail.com>
2022-04-22 19:03:56 +0530

    Test Case for Adding 1tonne and 1000gm gives 1000Kg

--- src/test/java/com/java/QuantityWeightsTest.java
+++ src/test/java/com/java/QuantityWeightsTest.java
@@ -15,13 +15,21 @@ public class QuantityWeightsTest {
 		Assert.assertEquals(value1, value2, 0.0);
 	}
 
-	/* TC 3.2:* 1 tonne = 1000 kgs */
+	/* TC 3.2: 1 tonne = 1000 kgs */
 	@Test
 	public void given1TonneAnd1000Kg_ShouldReturnEqual() {
 
 		double value1 = quantityMeasurement.unitConvert(WeightsUnits.TONNE, 1.0);
 		double value2 = quantityMeasurement.unitConvert(WeightsUnits.KILOGRAM, 1000);
 		Assert.assertEquals(value1, value2, 0.0);
+	}
+
+	/* TC 3.3 : 1 tonne + 1000 gm = 1001 kg */
+	@Test
+	public void given1TonneAnd1000Gram_WhenAdd_ShouldReturn_1000Kg() {
+		double value1 = quantityMeasurement.unitAdd(WeightsUnits.TONNE, 1.0, WeightsUnits.GRAM, 1000);
+		double value2 = quantityMeasurement.unitConvert(WeightsUnits.KILOGRAM, 1001);
+		Assert.assertEquals(value2, value1, 0.0);
 	}
 
 }
